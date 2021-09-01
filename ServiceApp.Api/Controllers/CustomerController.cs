ac3f8ef50ba0327f6c728dad8c5ba34c6f8ab63a
aboubacar diaby <ab.diaby@gmail.com>
2021-09-01 17:22:11 -0500

    version3.0

--- ServiceApp.Api/Controllers/CustomerController.cs
+++ ServiceApp.Api/Controllers/CustomerController.cs
@@ -13,7 +13,7 @@ namespace ServiceApp.Api.Controllers
     {
         public string Get()
         {
-            return "It is working. Version:2.0";
+            return "It is working. Version:3.0";
         }
     }
 }
